7e62b1c36020b9b70872bf3c3ed3f6bbbcce2aa4
Aleksander Sapieha <129301816+Aleksander-Sapieha@users.noreply.github.com>
2026-04-01 18:29:35 +0200

    Add files via upload

--- Mario Bros.lua
+++ Mario Bros.lua
@@ -1,8 +1,8 @@
 local settings = {
     split_on_stage_increase = false, -- Set to true to split on stage increase,
-    split_on_score_increase = true, -- Set to true to split on score increase,
-    target_score = 10, -- Set the target score for splitting, only used if split_on_score_increase is true
-    target_score_multiplier = 2, -- Multiplier for the target score after each split, only used if split_on_score_increase is true
+    -- split_on_score_increase = true, -- Set to true to split on score increase,
+    -- target_score = 1000, -- Set the target score for splitting, only used if split_on_score_increase is true
+    -- target_score_multiplier = 2, -- Multiplier for the target score after each split, only used if split_on_score_increase is true
     auto_start = true -- Set to true to automatically start the timer when the game starts
 }
 
@@ -35,17 +35,26 @@ while true do
         lastStage = stage
     end
     
-    if settings.split_on_score_increase then
-        local score = memory.readbyte(0x0095) * 100 + memory.readbyte(0x0096) * 10 + memory.readbyte(0x0097)
-        if score > settings.target_score then
-            pipe:write("split\n")
-            pipe:flush()
-            print("SPLIT")
-            score_multiplied = settings.target_score * settings.target_score_multiplier
-             -- Reset the target score for the next split
-            settings.target_score = score_multiplied
-        end
-    end
+    -- if settings.split_on_score_increase then
+    --     if pipe then
+    --         if started then
+    --             local score = ((memory.readbyte(0x0095) - 0x030) * 100 + (memory.readbyte(0x0096) - 0x030) * 10 + (memory.readbyte(0x0097) - 0x030)) / 1.6
+    --             print("Score:", score, "Target Score:", settings.target_score)
+    --             -- print("Address 0x0095:", memory.readbyte(0x0095), "Address 0x0096:", memory.readbyte(0x0096), "Address 0x0097:", memory.readbyte(0x0097))
+    --             if score > settings.target_score then
+    --                 pipe:write("split\n")
+    --                 pipe:flush()
+    --                 print("SPLIT")
+    --                 score_multiplied = settings.target_score * settings.target_score_multiplier
+    --                 print("New target score:", score_multiplied)
+    --                 -- Reset the target score for the next split
+    --                 settings.target_score = score_multiplied
+    --             end
+    --         end
+    --     else
+    --         print("PIPE NOT OPEN")
+    --     end
+    -- end
 
     emu.frameadvance()
 end
