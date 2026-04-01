b78b4d297c3b12d442177ec47e581a21aa8e8caa
Aleksander Sapieha <129301816+Aleksander-Sapieha@users.noreply.github.com>
2026-04-01 17:29:37 +0200

    Update 1.1

--- Mario Bros.lua
+++ Mario Bros.lua
@@ -1,3 +1,11 @@
+local settings = {
+    split_on_stage_increase = false, -- Set to true to split on stage increase,
+    split_on_score_increase = true, -- Set to true to split on score increase,
+    target_score = 10, -- Set the target score for splitting, only used if split_on_score_increase is true
+    target_score_multiplier = 2, -- Multiplier for the target score after each split, only used if split_on_score_increase is true
+    auto_start = true -- Set to true to automatically start the timer when the game starts
+}
+
 local pipe = io.open("\\\\.\\pipe\\LiveSplit", "w")
 
 local lastStage = memory.readbyte(0x0550)
@@ -10,13 +18,13 @@ while true do
         print("Stage:", lastStage, "->", stage)
 
         if pipe then
-            if not started and lastStage == 0 and stage == 1 or lastStage == 34 and stage == 1 then
+            if not started and lastStage == 0 and stage == 1 or lastStage == 34 and stage == 1 and settings.auto_start then
                 pipe:write("starttimer\n")
                 pipe:flush()
                 started = true
                 print("START")
 
-            elseif started then
+            elseif started and settings.split_on_stage_increase then
                 pipe:write("split\n")
                 pipe:flush()
                 print("SPLIT")
@@ -24,8 +32,19 @@ while true do
         else
             print("PIPE NOT OPEN")
         end
-
         lastStage = stage
+    end
+    
+    if settings.split_on_score_increase then
+        local score = memory.readbyte(0x0095) * 100 + memory.readbyte(0x0096) * 10 + memory.readbyte(0x0097)
+        if score > settings.target_score then
+            pipe:write("split\n")
+            pipe:flush()
+            print("SPLIT")
+            score_multiplied = settings.target_score * settings.target_score_multiplier
+             -- Reset the target score for the next split
+            settings.target_score = score_multiplied
+        end
     end
 
     emu.frameadvance()
